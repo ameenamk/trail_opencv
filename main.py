28fecc6d12af2fe34d106b92f592700cb7f565fa
ameena_mk <ameenamekkarakunnan@gmail.com>
2025-07-30 20:12:48 +0530

    Fixed OpenCV import error and updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from opencv!")
+import cv2
+from cvzone.HandTrackingModule import HandDetector
 
-
-if __name__ == "__main__":
-    main()
+cap=cv2.VideoCapture(0)
+cap.set(3,1000)
+cap.set(4,720)
